39c35596542bd887d92dfcadda717b9d48203de1
killinux <killinux@163.com>
2019-12-04 19:08:32 +0800

    webassembly run.sh

--- webassembly/usefile/run.sh
+++ webassembly/usefile/run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+# file:
+#emcc packfile.cc -o packfile.js --preload-file hello.txt
+
 python /opt/mt/emsdk/emscripten/incoming/tools/file_packager.py fp.data --preload dat_dir --js-output=fp.js
 emcc packdir.cc -o packdir_sep.js -s FORCE_FILESYSTEM=1
+
+#idbfs:
+#emcc idbfs.cc -lidbfs.js -o idbfs.js
